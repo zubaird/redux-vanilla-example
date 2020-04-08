17190f205277879c5e00e3ca187171271cba93e3
Zubair Desai <zubair.desai@gmail.com>
2020-04-08 11:02:36 -0500

    update readme & comments

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,12 +1,57 @@
 // /* eslint-env browser, es6 */
 import { createStore } from 'redux';
 import { combineReducers } from 'redux';
+
+// Part 1:
 // Useless counter elements
 const moreButtonElement = document.getElementById('more');
 const fewerButtonElement = document.getElementById('fewer');
 const uselessCounterElement = document.getElementById('uselessCounter');
 
+// Part 2:
 // Todo list elements
-const newTask = document.getElementById('newTask');
-const addTaskButton = document.getElementById('addTask');
-const taskList = document.getElementById('list');
+// const newTask = document.getElementById('newTask');
+// const addTaskButton = document.getElementById('addTask');
+// const taskList = document.getElementById('list');
+
+// ====REDUCER======
+// How to update the state based on action
+function uselessCounterReducer(state = 0, action) {
+  switch (action.type) {
+    case 'MORE':
+      return state + 1;
+    case 'FEWER':
+      return state < 1 ? 0 : state - 1;
+    default:
+      return state;
+  }
+}
+
+// Create a new REDUX store, and register reducers
+const store = createStore(combineReducers({ uselessCounterReducer }));
+
+// ====ACTIONS======
+// What to do when MORE is clicked:
+// Send send the ACTION object to the REDUCER with type "MORE"
+moreButtonElement.addEventListener('click', () => {
+  store.dispatch({ type: 'MORE' });
+});
+
+// What to do when FEWER is clicked:
+// Send send the ACTION object to the REDUCER with type "FEWER"
+fewerButtonElement.addEventListener('click', () => {
+  store.dispatch({ type: 'FEWER' });
+});
+
+
+// ====SUBSCRIBE======
+// What to do when there is a new state
+function updateTheDOM() {
+  const currentState = store.getState();
+  console.log(currentState);
+
+  uselessCounterElement.textContent = currentState.uselessCounterReducer;
+}
+
+// Run updateTheDOM whenever there is a new state in store.
+store.subscribe(updateTheDOM);
--- readme.md
+++ readme.md
@@ -1,3 +1,26 @@
 ### Simply Redux /w Plain Javascript
 
+
+Everything is loaded for you, this example includes Redux.
+
+*Get started*
+ - Terminal:
+   - `npm install`
+   - `npm run build`
+   - In a seperate tab: `npm start`  (you might need to install `live-server`)
+ - Work in `app.js`
+
+**Part 1: Create Useless Counter Walkthrough**
+
+ - Has 2 buttons: MORE and FEWER  (You already have the elements in app.js)
+ - Each has a click event that dispatches an ACTION object to the REDUCER
+ - The ACTION is what to do.
+ - The REDUCER is how to do it. It is registered to the Redux STORE when the store is created
+ - Custom UpdateTheDOM function is what do when there is a state change
+ - SUBSCRIBE (store.subscirb)
+
+**Part 2: Create todo list**
+
+ - Use the basics from part 1 to try to make a todo list.
+
 ![](https://i.gyazo.com/b71e8f0d42a06e607049a9caea7254a8.png)
